c3e75053dfdc7a6ff2808940424f061e930cfe3f
Takashi Mizohata <beatak@leto.local>
2011-12-02 09:23:11 -0500

    boolean test notation

--- test/boolean.test.js
+++ test/boolean.test.js
@@ -4,44 +4,44 @@ assert = require('assert');
 module.exports = {
   'test boolean:true' : function () {
     assert.equal(true, cast.boolean(true));
-  },
-  'test boolean:false' : function () {
+  }
+  ,'test boolean:false' : function () {
     assert.equal(false, cast.boolean(false));
-  },
-  'test function' : function () {
+  }
+  ,'test function' : function () {
     assert.equal(true, cast.boolean(function (){}));
-  },
-  'test number:0' : function () {
+  }
+  ,'test number:0' : function () {
     assert.equal(false, cast.boolean(0));
-  },
-  'test number:1' : function () {
+  }
+  ,'test number:1' : function () {
     assert.equal(true, cast.boolean(1));
-  },
-  'test object:null' : function () {
+  }
+  ,'test object:null' : function () {
     assert.equal(false, cast.boolean(null));
-  },
-  'test object:others' : function () {
+  }
+  ,'test object:others' : function () {
     assert.equal(true, cast.boolean({}));
-  },
-  'test string:false' : function () {
+  }
+  ,'test string:false' : function () {
     assert.equal(false, cast.boolean('false'));
-  },
-  'test string:0' : function () {
+  }
+  ,'test string:0' : function () {
     assert.equal(false, cast.boolean('0'));
-  },
-  'test string:undefined' : function () {
+  }
+  ,'test string:undefined' : function () {
     assert.equal(false, cast.boolean('undefined'));
-  },
-  'test string:null' : function () {
+  }
+  ,'test string:null' : function () {
     assert.equal(false, cast.boolean('null'));
-  },
-  'test string:empty' : function () {
+  }
+  ,'test string:empty' : function () {
     assert.equal(false, cast.boolean(''));
-  },
-  'test string:others' : function () {
+  }
+  ,'test string:others' : function () {
     assert.equal(true, cast.boolean('OH HAI'));
-  },
-  'test undefined' : function () {
+  }
+  ,'test undefined' : function () {
     assert.equal(false, cast.boolean(undefined));
   }
 };
